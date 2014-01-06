8c4b27e0db650062edb5cf2457c07a1572604890
Bing Xie <bing.xie78@gmail.com>
2014-01-06 15:18:43 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnMina::Application.config.secret_key_base = '8c71a9e35d78561aec8fe6817526f7289f5da11afd36f1616939b29d9c37d2e5ae38b221dcd262b3eaa32d99b0b80d7b017395b4bdea28877d92a2358ba5fd4a'
